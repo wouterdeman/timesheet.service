89640f6d901fea7033f84981ccb26c4af2be83d6
Friso De Ridder <frisoderidder@gmail.com>
2013-12-11 23:22:20 +0100

    Auhenticate with bite account
    Enabled passport-google -> added customcallback so that only bite
    accounts are authenticated

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var db = require('./mongoose');
 var express = require('express');
 var app = express();
 var port = 3000;
-
+var passport = require('passport');
+var GoogleStrategy = require('passport-google').Strategy;
 /*
  * Use Handlebars for templating
  */
@@ -55,9 +56,96 @@ app.set('view engine', 'handlebars');
 // Set bodyParser
 app.use(express.bodyParser());
 
+//app.use(express.session({secret: 'SECRET'}));
+/*
+* Configure passport
+*/
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+    returnURL: 'http://localhost:3000/auth/google/return',
+    realm: 'http://localhost:3000/'
+  },
+  function(identifier, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      profile.identifier = identifier;
+      return done(null, profile);
+    });
+  }
+));
+
+var authenticateBiteAccount = function(req, success, failure){
+    return passport.authenticate('google', function(err, user, info){
+        if(err){
+            failure(err);
+        }
+        else if(!user){
+            failure('user does not exist');
+        }
+        else{
+            if(user.emails[0].value.indexOf('@bite.be') === -1){
+                failure('user must be a bite account');
+            }
+            else{
+                req.login(user, function(err){
+                    if(err){
+                        failure(err);    
+                    }else{
+                        success();
+                    }
+                    
+                });
+            }
+        }
+    });
+};
+
+var biteAuthMiddleware = function(req, res, next){
+    var success = function(){
+        res.redirect('/')
+    };
+
+    var failure = function(error){
+        res.error = error;
+        res.redirect('/login');
+    }
+
+    var auth = authenticateBiteAccount(req, success, failure);
+    auth(req, res, next);
+};
+
+app.use( express.cookieParser() );
+  app.use(express.session({ secret: 'veryverysecret' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+
 /*
  * Routes
  */
+
+app.get('/auth/google', passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+    res.redirect('/');
+});
+
+app.get('/auth/google/return', biteAuthMiddleware);
+
+/*
+app.get('/auth/google/return', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+*/
+
 require('./routes')(app);
 
 /*
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 module.exports = function(app) {
-	app.get('/', function(request, response, next) {
-		response.render('index');
+	app.get('/', ensureAuthenticated, function(req, res) {
+		res.render('index', { user: req.user });
+	});
+
+	app.get('/login', function(req, res){
+    	res.render('logon', { user: req.user, errorMessage: req.errorMessage });
+	}); 
+
+	app.get('/logout', function(req, res){
+	  req.logout();
+	  res.redirect('/login');
 	});
 
 	require('./entrystore')(app);
@@ -8,4 +17,11 @@ module.exports = function(app) {
 	require('./customer')(app);
 	// other routes entered here as require(route)(app);
 	// we basically pass 'app' around to each route
+
+	function ensureAuthenticated(req, res, next) {
+		if (req.isAuthenticated()) { 
+			return next(); }
+		res.redirect('/login')
+	}
+
 }
